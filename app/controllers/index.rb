907ea8499f803f7f7516d216c5d97e176d644a13
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-09 18:05:08 -0700

    got it to work...site is extremely ugly

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -19,11 +19,13 @@ get '/auth' do
   session.delete(:request_token)
 
   # at this point in the code is where you'll need to create your user account and store the access token
-  if !User.find_by_username(@access_token.params[:screen_name])
-    User.create(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
-  end
-  redirect '/new_tweet'
+  user = User.find_by_username(@access_token.params[:screen_name])
   
+  if !user
+    user = User.create(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  end
+  session[:user_id] = user.id
+  redirect '/new_tweet'
 end
 
 get '/new_tweet' do
@@ -31,7 +33,14 @@ get '/new_tweet' do
 end
 
 post '/new_tweet' do
+  user = User.find(session[:user_id])
+  puts '---------'
+  puts user.id
+  puts '---------'
   tweet_content = params[:tweet_content]
-  tweet = Twitter.update(tweet_content)
+  tweet = Twitter::Client.new(
+  :oauth_token => user.oauth_token,
+  :oauth_token_secret => user.oauth_secret
+  ).update(tweet_content)
   tweet.class == Twitter::Tweet ? "Success!" : "Something went wrong..."
 end
